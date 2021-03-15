d7d24a7cf7b8dcf616bb9b804253aa9e7ba1bc44
Oleh <alik49410@gmail.com>
2021-03-15 15:59:44 +0200

    listen from textview and creating file

--- app/src/main/java/ru/astar/btarduinoapp/MainActivity.java
+++ app/src/main/java/ru/astar/btarduinoapp/MainActivity.java
@@ -47,7 +47,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
-public class MainActivity extends AppCompatActivity implements
+public class    MainActivity extends AppCompatActivity implements
         CompoundButton.OnCheckedChangeListener,
         AdapterView.OnItemClickListener,
         View.OnClickListener {
@@ -125,7 +125,6 @@ public class MainActivity extends AppCompatActivity implements
         graph = (GraphView) findViewById(R.id.graphView);
 
 
-
         switchEnableBt.setOnCheckedChangeListener(this);
         btnEnableSearch.setOnClickListener(this);
         listBtDevices.setOnItemClickListener(this);
@@ -160,6 +159,30 @@ public class MainActivity extends AppCompatActivity implements
             switchEnableBt.setChecked(true);
             setListAdapter(BT_BOUNDED);
         }
+
+
+        btn_send_message.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (et_input_comand.getText().toString().length() > 0) {
+                    connectedThread.writeMesToBlu(et_input_comand.getText().toString());
+                    Toast.makeText(MainActivity.this, R.string.succ_send, Toast.LENGTH_SHORT).show();
+                    et_input_comand.setText("");
+                    etConsole.setText("");
+                }
+            }
+        });
+
+        btn_create_graph.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String dataFromBl = etConsole.getText().toString();
+                save(dataFromBl);
+                graph.setVisibility(View.VISIBLE);
+                drawGraph();
+
+            }
+        });
     }
 
     @Override
@@ -180,7 +203,7 @@ public class MainActivity extends AppCompatActivity implements
     public void save(String text) {
         FileOutputStream fos = null;
         try {
-            fos = openFileOutput(FILE_NAME , MODE_PRIVATE);
+            fos = openFileOutput(FILE_NAME, MODE_PRIVATE);
             fos.write(text.getBytes());
             Toast.makeText(this, "Saved to " + getFilesDir() + "/" + FILE_NAME,
                     Toast.LENGTH_LONG).show();
@@ -200,26 +223,50 @@ public class MainActivity extends AppCompatActivity implements
     }
 
 
-    int[][] readFile(){
-        int[][] intsAll = new int[30000][3];
-        int i =0;
+    int[][] readFile() {
+        int[][] intsAll=null ;
+        int i = 0;
+        String [] dataAr=new String [2];
         FileInputStream fis = null;
         BufferedReader br = null;
-        String data="";
+        List<String > list=new ArrayList();
+//        InputStreamReader isr=null;
+        InputStreamReader isr=null;
+        String data = "";
         StringBuilder sb=new StringBuilder();
         try {
             fis = openFileInput(FILE_NAME);
-            InputStreamReader isr = new InputStreamReader(fis);
+             isr = new InputStreamReader(fis);
             br = new BufferedReader(isr);
+
+//             isr = this.getResources().openRawResource(R.raw.log);
+//            br = new BufferedReader(new InputStreamReader(isr));
+
             while ((data = br.readLine()) != null) {
-//                sb.append(data).append("\n");
-                intsAll[i][0] = Integer.parseInt(data.split(" ")[0]);
-                intsAll[i][2] = Integer.parseInt(data.split(" ")[1]);
-//                intsAll[i][1] = Integer.parseInt(data.substring(6, 11));
-//                intsAll[i][2] = Integer.parseInt(data.substring(12, 17));
                 i++;
+                list.add(data);
             }
+
+//
+
+//             isr = this.getResources().openRawResource(R.raw.log);
+//            br = new BufferedReader(new InputStreamReader(isr));
+
+
+            intsAll = new int[i+1][2];
+            int j=0;
+            while (j<i-1) {
+                data=list.get(j);
+                sb.append(data).append("\n");
+                dataAr=data.split(" ");
+                intsAll[j][0] = Integer.parseInt(dataAr[0]);
+                intsAll[j][1] = Integer.parseInt(dataAr[1]);
+                j++;
+            }
+            Toast.makeText(this, "Readed lines "+j + getFilesDir() + "/" + FILE_NAME,
+                    Toast.LENGTH_LONG).show();
         } catch (IOException e) {
+            Toast.makeText(this,"e"+e,Toast.LENGTH_LONG);
             e.printStackTrace();
         } finally {
             if (fis != null) {
@@ -230,8 +277,9 @@ public class MainActivity extends AppCompatActivity implements
                 }
             }
         }
-        numberOfElements=i;
-        return   intsAll;
+//        save(sb.toString());
+        numberOfElements = i;
+        return intsAll;
     }
 
 
@@ -247,18 +295,7 @@ public class MainActivity extends AppCompatActivity implements
             if (connectThread != null) {
                 connectThread.cancel();
             }
-
             showFrameControls();
-        } else if (v.equals(btn_send_message)) {
-            connectedThread.writeMesToBlu(et_input_comand.getText().toString());
-            et_input_comand.setText("");
-            etConsole.setText("");
-
-        } else if (v.equals(btn_create_graph)) {
-            String dataFromBl=etConsole.getText().toString();
-            save(dataFromBl);
-            graph.setVisibility(View.VISIBLE);
-            drawGraph();
         }
     }
 
@@ -358,7 +395,6 @@ public class MainActivity extends AppCompatActivity implements
                 tmpArrayList.add(device);
             }
         }
-
         return tmpArrayList;
     }
 
@@ -424,7 +460,6 @@ public class MainActivity extends AppCompatActivity implements
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
         switch (requestCode) {
             case REQUEST_CODE_LOC:
-
                 if (grantResults.length > 0) {
                     for (int gr : grantResults) {
                         // Check if request is granted or not
@@ -461,11 +496,9 @@ public class MainActivity extends AppCompatActivity implements
             try {
                 bluetoothSocket.connect();
                 success = true;
-
                 progressDialog.dismiss();
             } catch (IOException e) {
                 e.printStackTrace();
-
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
@@ -476,7 +509,6 @@ public class MainActivity extends AppCompatActivity implements
 
                 cancel();
             }
-
             if (success) {
                 connectedThread = new ConnectedThread(bluetoothSocket);
                 connectedThread.start();
@@ -600,9 +632,10 @@ public class MainActivity extends AppCompatActivity implements
 //            connectedThread.write(command);
         }
     }
+
     public double SaO2(double a) {
         Log.i("saO2A", a + "");
-        double rez = (1.13 - (a / 3));
+        double rez = (1.03 - (a / 3));
         Log.i("saO2rez", rez + "");
         return rez;
     }
@@ -721,10 +754,10 @@ public class MainActivity extends AppCompatActivity implements
         Log.i("aSao2", a + " ");
         Log.i("r1Sao2", r1 + " ");
         Log.i("arLeSao2", XXandXYandYYDoubles.length + " ");
-        if (r1 >= .99) {
+//        if (r1 >= .99) {
             return SaO2(a);
-        } else
-            return 0;
+//        } else
+//            return 0;
     }
 
     public double DiscoverSaCo(int[][] ints) {
@@ -747,10 +780,7 @@ public class MainActivity extends AppCompatActivity implements
     }
 
 
-
-
-
-    public  void drawGraph() {
+    public void drawGraph() {
 //        String path =getFilesDir().toString();
 //        Log.d("Files", "Path: " + path);
 //        File directory = new File(path);
@@ -761,42 +791,45 @@ public class MainActivity extends AppCompatActivity implements
 //            Log.d("Files", "FileName:" + files[i].getName());
 //        }
 //
-        int [][] intsAll=readFile();
-        int i=numberOfElements;
+        int[][] intsAll = readFile();
+        int i = numberOfElements;
 
         double[] doublesSo2 = new double[i / 200];
         int[][] ints = new int[200][2];
+        List <Double> listSo2=new ArrayList();
         for (int j = 0; j < i / 200; j++) {
             for (int k = 0; k < 200; k++) {
                 ints[k][0] = intsAll[k + j * 200][0];
-                ints[k][1] = intsAll[k + j * 200][2];
+                ints[k][1] = intsAll[k + j * 200][1];
             }
             doublesSo2[j] = (DiscoverSaO2(ints));
             Log.i("doublesSo2DrawGraph", doublesSo2[j] + "");
             if (doublesSo2[j] == 0) {
                 if (j > 1) {
+                    listSo2.add( doublesSo2[j - 1]);
                     doublesSo2[j] = doublesSo2[j - 1];
                 }
             }
+
 //            Log.i("doublesSo2DrawGraphAfter", doublesSo2[j] + "");
         }
 
-        double[] doublesSaCO = new double[i / 200];
-        int[][] ints2 = new int[200][2];
-        for (int j = 0; j < i / 200; j++) {
-            for (int k = 0; k < 200; k++) {
-                ints2[k][0] = intsAll[k + j * 200][1];
-                ints2[k][1] = intsAll[k + j * 200][2];
-            }
-            doublesSaCO[j] = (DiscoverSaCo(ints2));
-            Log.i("doublesSaCODrawGraph", doublesSaCO[j] + "");
-            if (doublesSaCO[j] == 0) {
-                if (j > 1) {
-                    doublesSaCO[j] = doublesSaCO[j - 1];
-                }
-            }
-//            Log.i("doublesSaCODrawGraphAfter", doublesSaCO[j] + "");
-        }
+//        double[] doublesSaCO = new double[i / 200];
+//        int[][] ints2 = new int[200][2];
+//        for (int j = 0; j < i / 200; j++) {
+//            for (int k = 0; k < 200; k++) {
+//                ints2[k][0] = intsAll[k + j * 200][1];
+//                ints2[k][1] = intsAll[k + j * 200][2];
+//            }
+//            doublesSaCO[j] = (DiscoverSaCo(ints2));
+//            Log.i("doublesSaCODrawGraph", doublesSaCO[j] + "");
+//            if (doublesSaCO[j] == 0) {
+//                if (j > 1) {
+//                    doublesSaCO[j] = doublesSaCO[j - 1];
+//                }
+//            }
+////            Log.i("doublesSaCODrawGraphAfter", doublesSaCO[j] + "");
+//        }
 //        textViewArray.setText("");
 
         graph.setVisibility(View.VISIBLE);
@@ -807,28 +840,29 @@ public class MainActivity extends AppCompatActivity implements
             DataPoint point = new DataPoint(i2, doublesSo2[i2] * 100);
             series.appendData(point, true, size);
         }
-        for (int i2 = 10; i2 < size; i2++) {
-            DataPoint pointF = new DataPoint(i2, doublesSaCO[i2] * 100);
-            series2.appendData(pointF, true, size);
-        }
-        graph.getViewport().setMinX(10  );
+//        for (int i2 = 10; i2 < size; i2++) {
+//            DataPoint pointF = new DataPoint(i2, doublesSaCO[i2] * 100);
+//            series2.appendData(pointF, true, size);
+//        }
+        graph.getViewport().setMinX(10);
         graph.getViewport().setMaxX(size);
         graph.getViewport().setMinY(-10);
         graph.getViewport().setMaxY(110);
         graph.getViewport().setYAxisBoundsManual(true);
         graph.getViewport().setXAxisBoundsManual(true);
-        series.setColor(Color.BLACK);
-        series2.setColor(Color.RED);
+        series.setColor(Color.RED);
+//        series2.setColor(Color.RED);
         graph.addSeries(series);
-        graph.addSeries(series2);
-        double sred=0;
-        for (int l=0;l<doublesSaCO.length;l++){
-            sred+=doublesSaCO[l];
+//        graph.addSeries(series2);
+        double sred = 0;
+        for (int l = 0; l < doublesSo2.length; l++) {
+            sred += doublesSo2[l];
         }
-        sred=sred/doublesSaCO.length;
-        Log.i("sredCheck",sred+"");
-        if (sred>0.10) {
-            Toast.makeText(this,"Терміново к доктору", Toast.LENGTH_LONG).show();
-        }
+        sred = sred / doublesSo2.length;
+        Log.i("sredCheck", sred + "");
+        Toast.makeText(this, "среднее "+sred, Toast.LENGTH_LONG).show();
+//        if (sred > 0.10) {
+//            Toast.makeText(this, "среднее "+sred, Toast.LENGTH_LONG).show();
+//        }
     }
 }
